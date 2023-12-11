442a750975b47a16854e062c09c9699a2cdcc58a
PasLoin <25326871+PasLoin@users.noreply.github.com>
2023-12-11 17:55:02 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -492,26 +492,27 @@ function showReportOut() {
 }
 
 function showStatus() {
-	if (startnode != null) {
-		let textx = 2;
-		let texty = mapHeight - 400;
-		fill(0, 5, 225);
-		noStroke();
-		textSize(12);
-		textAlign(LEFT);
-		text("Total number nodes: " + nodes.length, textx, texty);
-		text("Total number road sections: " + edges.length, textx, texty + 20);
-		if (bestroute != null) {
-			if (bestroute.waypoints.length > 0) {
-				text("Best route: " + nf(bestroute.distance, 0, 3) + "km, " + nf(100 * totaledgedistance / bestroute.distance, 0, 2) + "%", textx, texty + 60);
-			}
-			text("Routes tried: " + iterations, textx, texty + 80);
-			text("Frame rate: " + frameRate(), textx, texty + 100);
-			text("Solutions per frame: " + iterationsperframe, textx, texty + 120);
-			text("Iterations/second: " + iterations / (millis() - starttime) * 1000, textx, texty + 140);
-			text("best routes: " + efficiencyhistory.length, textx, texty + 160);
-			text("efficiency gains: " + nf(100 * totalefficiencygains, 0, 2) + "% and " + nf(100 * totalefficiencygains / (millis() - starttime) * 1000, 0, 2) + "% gains/sec:", textx, texty + 180); //
-			text("isTouchScreenDevice: " + showEdges(), textx, texty + 200);		
-		}
-	}
+    if (startnode != null) {
+        let textx = 2;
+        let texty = mapHeight - 400;
+        fill(0, 5, 225);
+        noStroke();
+        textSize(12);
+        textAlign(LEFT);
+        text("Total number nodes: " + nodes.length, textx, texty);
+        text("Total number road sections: " + edges.length, textx, texty + 20);
+        if (bestroute != null) {
+            if (bestroute.waypoints.length > 0) {
+                text("Best route: " + nf(bestroute.distance, 0, 3) + "km, " + nf(100 * totaledgedistance / bestroute.distance, 0, 2) + "%", textx, texty + 60);
+            }
+            text("Routes tried: " + iterations, textx, texty + 80);
+            text("Frame rate: " + frameRate(), textx, texty + 100);
+            text("Solutions per frame: " + iterationsperframe, textx, texty + 120);
+            text("Iterations/second: " + iterations / (millis() - starttime) * 1000, textx, texty + 140);
+            text("best routes: " + efficiencyhistory.length, textx, texty + 160);
+            text("efficiency gains: " + nf(100 * totalefficiencygains, 0, 2) + "% and " + nf(100 * totalefficiencygains / (millis() - starttime) * 1000, 0, 2) + "% gains/sec:", textx, texty + 180);
+            text("isTouchScreenDevice: " + showEdges(), textx, texty + 200);
+            text("Remaining Edges: " + remainingedges, textx, texty + 220); // Add this line to display remainingedges
+        }
+    }
 }
